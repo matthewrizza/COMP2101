e0cb6beb1d64f20b0a0ac3048a850b833bf92fe7
Matthew Rizza <matthewrizza@outlook.com>
2020-10-12 23:10:40 -0700

    Update.

--- bash/hostname-changer.sh
+++ bash/hostname-changer.sh
@@ -8,11 +8,7 @@
 # Get the current hostname using the hostname command and save it in a variable
 currentHostname=$(hostname)
 # Tell the user what the current hostname is in a human friendly way
-<<<<<<< HEAD
-echo "Your current hostname is: $hostname"
-=======
 echo "Your current hostname is: $currentHostname."
->>>>>>> 43c3f04efe0327ea627ebcedbd0e27a66688027f
 # Ask for the user's student number using the read command
 read -p "Please enter your student number: " studentNumber
 # Use that to save the desired hostname of pcNNNNNNNNNN in a variable, where NNNNNNNNN is the student number entered by the user
@@ -20,7 +16,6 @@ desiredHostname=pc$studentNumber
 # If that hostname is not already in the /etc/hosts file, change the old hostname in that file to the new name using sed or something similar and
 #     tell the user you did that
 #e.g. sed -i "s/$oldname/$newname/" /etc/hosts
-<<<<<<< HEAD
 grep $desiredHostname /etc/hosts || (printf "$desiredHostname Does not already exist.\n Changing hostname to $desiredHostname." &&
 sudo sed -i "s/$currentHostname/$desiredHostname/" /etc/hosts)
 # If that hostname is not the current hostname, change it using the hostnamectl command and
@@ -28,10 +23,3 @@ sudo sed -i "s/$currentHostname/$desiredHostname/" /etc/hosts)
 #e.g. hostnamectl set-hostname $newname
 test $currentHostname == $desiredHostname && echo "Your hostname is already named $desiredHostname." ||
 hostnamectl set-hostname $desiredHostname && echo "Please restart your machine for the changes to take full effect."
-=======
-grep $desiredHostname /etc/hosts || (printf "$desiredHostname Does not already exist.\n Changing hostname to $desiredHostname." && sed -i "s/$currentHostname/$desiredHostname/" /etc/hosts)
-# If that hostname is not the current hostname, change it using the hostnamectl command and
-#     tell the user you changed the current hostname and they should reboot to make sure the new name takes full effect
-#e.g. hostnamectl set-hostname $newname
-test $currentHostname == $desiredHostname && echo "Your hostname is already named $desiredHostname." || hostnamectl set-hostname $desiredHostname && echo "Please restart your machine for the changes to take full effect."
->>>>>>> 43c3f04efe0327ea627ebcedbd0e27a66688027f
