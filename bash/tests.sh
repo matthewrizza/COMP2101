0c6fbcc58dc674a9e53c43244f09e2d872e26bf9
Matthew Rizza <matthewrizza@outlook.com>
2020-11-05 19:48:00 -0800

    Finished tests.sh

--- bash/tests.sh
+++ bash/tests.sh
@@ -60,10 +60,11 @@ echo "Your second number is not a multile of your first number"
 # TASK 1: Add a command that prints out a labelled description of what is in the USER variable, but only if it is not empty
 # TASK 2: Add a command that tells the user if the USER variable exists, but is empty
 [ -v USER ] && echo "The variable SHELL exists"
-[ -s USER ] && echo $USER
+[ -n "$USER" ] && echo "Your username is $USER"
+[ -z "$USER" ] && echo "The USER variable exists, but is empty"
 # Tests for string data
 # TASK 3: Modify the command to use the != operator instead of the = operator, without breaking the logic of the command
 # TASK 4: Use the read command to ask the user running the script to give us strings to use for the tests
 a=1
 b=01
-[ $a = $b ] && echo "$a is alphanumerically equal to $b" || echo "$a is not alphanumerically equal to $b"
+[ ! $a != $b ] && echo "$a is alphanumerically equal to $b" || echo "$a is not alphanumerically equal to $b"
