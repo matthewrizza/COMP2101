1787107a0e5eb5ddaf9b2ee7b0aaff2e66731672
matthewrizza <71408997+matthewrizza@users.noreply.github.com>
2020-12-16 18:13:27 -0500

    Finished Lab #5

---
+++ powershell/systemReport-Parameter.ps1
@@ -0,0 +1,57 @@
+﻿param (
+        [switch]$System,
+        [switch]$Disks,
+        [switch]$Network
+)
+
+if ( $System -eq $false -and $Disks -eq $false -and $Network -eq $false ) {
+        systemReport.ps1
+}
+else {
+        Write-Output ""
+        Write-Output "System Information Report"
+        Write-Output "--------------------------"
+        Write-Output "
+        "
+        if ( $System -eq $true ) {
+    
+            Write-Output "Processor"
+            Write-Output "----------------------"
+            (Get-ProcessorInfo | Out-String).Trim()
+            Write-Output "
+
+            "    
+            Write-Output "Operating System"
+            Write-Output "----------------------"
+            (Get-SysInfo | Out-String).Trim()
+            Write-Output "
+    
+            "
+            Write-Output "Memory"
+            Write-Output "----------------------"
+            (Get-RAM | Out-String).Trim()
+            Write-Output "
+
+            "
+            Write-Output "Video Card"
+            Write-Output "----------------------"
+            (Get-VideoCard | Out-String).Trim()
+            Write-Output ""
+        }
+
+        if ( $Disks -eq $true ) {
+    
+            Write-Output "Physical Disk Drives"
+            Write-Output "----------------------"
+            (Get-Summary | Out-String).Trim()
+            Write-Output ""
+        }
+
+        if ( $Network -eq $true ) {
+
+            Write-Output "Network Adapter"
+            Write-Output "----------------------"
+            (Get-IPConfig | Out-String).Trim()
+            Write-Output ""
+        }
+}
